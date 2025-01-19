a4bea330b8cb789d4475ff6d0a790d51fc52b450
Azlan Khawar <113675447+azlan18@users.noreply.github.com>
2025-01-19 14:19:27 +0530

    Update BlogList.tsx

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -41,7 +41,7 @@ const BlogList: React.FC = () => {
 
   return (
     <div className="container mx-auto px-4 py-8 my-12">
-      <h1 className="text-3xl font-bold mb-8 text-center text-black font-mono">Blog Posts</h1>
+      <h1 className="text-3xl font-bold mb-8 text-center text-black font-mono">Blog Posts(Kindly only use Navbar to navigate. Do not refresh page)</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {blogs.map((blog) => (
           <BlogCard key={blog._id} blog={blog} />
